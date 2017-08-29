7991d409afb6e0559f4931791a3a41219fac6e38
MoreBlood <new.shtirlic@mail.ru>
2017-08-29 23:25:08 +0300

    добавил распознавание цвета нижней части картинки

--- routes/upload.js
+++ routes/upload.js
@@ -14,8 +14,11 @@ const error = require('rest-api-errors');
 
 const _ = require('lodash');
 const mime = require('mime-types');
+const ColorThief = require('color-thief');
+const sharp = require('sharp');
 
 const root = process.cwd();
+
 
 router.post('/', (req, res, next) => {
   if (!req.files) {
@@ -24,9 +27,15 @@ router.post('/', (req, res, next) => {
 
   const sampleFile = req.files.photo;
   const fileName = `${uuidv4()}.${mime.extension(sampleFile.mimetype)}`;
+  const fileUri = `${root}/public/images/${fileName}`;
+  const colorThief = new ColorThief();
 
-  return sampleFile.mv(`${root}/public/images/${fileName}`)
-    .then(() => res.json({ fileName }))
+  return sampleFile.mv(fileUri)
+    .then(() => sharp(fileUri)
+      .resize(null, 50)
+      .crop(sharp.gravity.south)
+      .toBuffer())
+    .then(image => res.json({ fileName, mainColor: `rgb(${colorThief.getColor(image).join(', ')})` }))
     .catch(err => next(new error.InternalServerError('FILE_SAVE_ERR', err)));
 });
 
