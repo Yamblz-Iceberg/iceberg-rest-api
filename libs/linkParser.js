9f9b153884f53de46dd87bb101db1ef50238cac3
MoreBlood <new.shtirlic@mail.ru>
2017-09-01 18:50:16 +0300

    новая библиотека для изображений

--- libs/linkParser.js
+++ libs/linkParser.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const request = require('request');
 const _ = require('lodash');
+const ImageResolver = require('image-resolver');
 
 const getInfo = url => new Promise((resolve, reject) => {
   request({ method: 'GET', url }, (err, response, body) => {
@@ -8,23 +9,34 @@ const getInfo = url => new Promise((resolve, reject) => {
       reject(err);
     }
     try {
+      const resolver = new ImageResolver();
+      resolver.register(new ImageResolver.FileExtension());
+      resolver.register(new ImageResolver.MimeType());
+      resolver.register(new ImageResolver.Opengraph());
+      resolver.register(new ImageResolver.Webpage());
+
       const httpRegEx = /http/;
       const excludeRegex = /logo/;
       const excludeClasses = [];
       const domain = `${response.request.uri.protocol}//${response.request.uri.host}`;
       const $ = cheerio.load(body);
-      const images = $('#content img, article img, main img').map(function (i, e) {
-        const srcPhoto = $(this).attr('src');
-        if (excludeRegex.test(srcPhoto) || $(this).hasClass('avatar')) {
-          return null;
-        }
-        return srcPhoto ? `${(httpRegEx.test(srcPhoto) ? '' : domain)}${srcPhoto}` : null;
-      }).get();
-      const name = $('title').text();
-      const photo = _.first(images);
-      const hrefFavicon = $('link[rel~=icon]').attr('href');
-      const favicon = (httpRegEx.test(hrefFavicon) ? '' : domain) + hrefFavicon;
-      resolve({ photo, name, favicon });
+      // const images = $('#content img, article img, main img').map(function (i, e) {
+      //   const srcPhoto = $(this).attr('src');
+      //   if (excludeRegex.test(srcPhoto) || $(this).hasClass('avatar')) {
+      //     return null;
+      //   }
+      //   return srcPhoto ? `${(httpRegEx.test(srcPhoto) ? '' : domain)}${srcPhoto}` : null;
+      // }).get();
+      resolver.resolve( url, function( result ){
+        let photo = null;
+        if (result) {
+          photo = result.image;
+        }  
+        const name = $('title').text();
+        const hrefFavicon = $('link[rel~=icon]').attr('href');
+        const favicon = (httpRegEx.test(hrefFavicon) ? '' : domain) + hrefFavicon;
+        resolve({ photo, name, favicon });
+      })
     } catch (error) {
       reject(error);
     }
