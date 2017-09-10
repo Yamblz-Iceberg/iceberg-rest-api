f9dbe2c2dc0f303dc247405943e5a5bfdd637f59
MoreBlood <new.shtirlic@mail.ru>
2017-09-10 16:48:51 +0300

    Вынес catch

--- routes/upload.js
+++ routes/upload.js
@@ -10,18 +10,18 @@ const Multer = require('multer');
 const multer = new Multer({
   storage: Multer.MemoryStorage,
   limits: {
-    fileSize: 10 * 1024 * 1024, // 10 мб
+    fileSize: 10 * 1024 * 1024,
   },
 });
 
 router.post('/', passport.authenticate('bearer', { session: false }), status.accountTypeMiddleware,
   multer.single('photo'), (req, res, next) =>
     postProccess.resize(req.file, 100)
-      .then(resizedImage => gcs.upload(resizedImage, '_islands100')
+      .then(resizedImage100 => gcs.upload(resizedImage100, '_islands100')
         .then(link100 => postProccess.resize(req.file, 1000)
           .then(resizedImage1000 => gcs.upload(resizedImage1000)
             .then(link1000 => postProccess.average(req.file)
-              .then(average => res.json({ link: link1000, linkIslands100: link100, mainColor: average })
-                .catch(err => next(err))))))));
+              .then(average => res.json({ link: link1000, linkIslands100: link100, mainColor: average }))))))
+      .catch(err => next(err)));
 
 module.exports = router;
