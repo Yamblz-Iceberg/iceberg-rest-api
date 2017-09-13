3c6eaf9aac537c33141fc444b80acebeba08520a
MoreBlood <new.shtirlic@mail.ru>
2017-09-13 18:32:47 +0300

    travis

--- libs/db/mongoose.js
+++ libs/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('../config');
 
 if (process.env.NODE_ENV === 'test') {
-  mongoose.connection = mongoose.createConnection(config.get('mongoose:uri_test'));
+  mongoose.connection = mongoose.createConnection(config.get('mongoose:uri_test') || process.env.MONGODB_URI_TEST);
 } else if (process.env.MONGODB_URI) {
   mongoose.connection = mongoose.createConnection(config.get('mongodb_uri') || process.env.MONGODB_URI);
 } else {
